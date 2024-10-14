24eb29d57850445e878cbf73a6286037fd8b9435
paj220001 <paj220001@utdallas.edu>
2024-10-14 00:54:23 -0500

    done I think

--- project3.asm
+++ project3.asm
@@ -128,7 +128,73 @@
 	la $a0, iterative
 	syscall
 	
+	la $s0, 0
+	lw $t1, x
+	lw $t2, y
+	addi $t2, $t2, 1
+	itr_sumsq:
+		mul $t3 , $t1 , $t1
+		
+		add $s0, $s0, $t3
+		
+		li $v0, SysPrintInt
+		move $a0, $s0
+		syscall
+		
+		li $v0, SysPrintChar
+		li $a0, ' '
+		syscall
+		
+		addi $t1, $t1, 1
+		bne $t1, $t2, itr_sumsq
+		
+	itr_exit: 
+	
+	li $v0, 11         # Print character syscall
+    	li $a0, '\n'        # Load space character
+    	syscall
+	
+	li $v0, SysPrintString 
+	la $a0, recursive
+	syscall
+	
+	la $s0, 0           # Initialize sum to 0 (s0 will hold the sum)
+    	lw $t1, x           # Load the value of x into $t1
+    	lw $t2, y           # Load the value of y into $t2
+    	addi $t2, $t2, 1    
+	
+	jal rec_sumsq
+	
+	
 	Exit:
 		li $v0, SysExit
 		Syscall
 	
+	rec_sumsq:
+    		# Base case: if t1 (current number) >= t2 (limit), return
+    		bge $t1, $t2, rec_exit
+
+    		# Calculate square of the current number
+    		mul $t3, $t1, $t1
+
+    		# Add the square to the sum in $s0
+    		add $s0, $s0, $t3
+
+    		# Print the current sum
+    		li $v0, 1          # Print integer syscall
+    		move $a0, $s0      # Move sum to $a0
+    		syscall
+
+    		# Print space character
+    		li $v0, 11         # Print character syscall
+    		li $a0, ' '        # Load space character
+    		syscall
+
+    		# Increment the current number (t1)
+    		addi $t1, $t1, 1
+
+    		# Recursive call
+    		jal rec_sumsq
+
+	rec_exit:
+   		jr $ra              # Return to caller
